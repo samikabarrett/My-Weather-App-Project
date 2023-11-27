dc74df450d68684272dbaef4acbb5f05ec33180b
Samika Barrett <150214612+samikabarrett@users.noreply.github.com>
2023-11-27 11:44:45 +1100

    Weather forecast - JS template / API / API integration

--- src/index.js
+++ src/index.js
@@ -18,6 +18,8 @@ location_on
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+
+  getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -51,7 +53,7 @@ function formatDate(date) {
 
 function searchCity(city) {
   let apiKey = "3ta98ca40d628aobcaf1ca49bf2be168";
-  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric&units=metric`;
   axios.get(apiURL).then(refreshWeather);
 }
 
@@ -60,6 +62,48 @@ function handleSearchSubmit(event) {
   let searchInput = document.querySelector("#search-form-input");
 
   searchCity(searchInput.value);
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  let forecastHtml = "";
+
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+
+        <img src="${day.condition.icon_url}" class="weather-forecast-emoji" />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature">
+            <strong>${Math.round(day.temperature.maximum)}º</strong> / 
+          </span>
+          <span class="weather-forecast-temperature">${Math.round(
+            day.temperature.minimum
+          )}º</span>
+        </div>
+      </div>
+    `;
+    }
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
 }
 
 let searchFormElement = document.querySelector("#search-form");
